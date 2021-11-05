be9281d7190c9607a02e4656ca7e1849b5014462
RKulagin <rus.kulagin2001@yandex.ru>
2021-11-05 13:17:09 +0300

    Add functions at and insert for basic functionality

--- include/binary_search_tree.hpp
+++ include/binary_search_tree.hpp
@@ -3,7 +3,8 @@
 #ifndef INCLUDE_BINARY_SEARCH_TREE_HPP_
 #define INCLUDE_BINARY_SEARCH_TREE_HPP_
 
-template<class Key, class Value, class Cmp = std::less<Key>>
+#include <memory>
+template <class Key, class Value, class Cmp = std::less<Key>>
 class BinarySearchTree {
  public:
   using key_type = Key;
@@ -11,20 +12,85 @@ class BinarySearchTree {
 
   // TODO: rule of five
 
+  void insert(key_type&& key, value_type&& value) {
+    auto place = find(key);
+    if (place == nullptr) {
+      root = std::make_shared<Node>(std::move(key), std::move(value));
+      return;
+    }
+    if (key == place->key) {
+      throw std::runtime_error(
+          "Binary Search Tree can't handle two nodes with similar key");
+    }
+    if (cmp(key, place->key)) {
+      place->left =
+          std::make_shared<Node>(std::move(key), std::move(value), place);
+    } else {
+      place->right =
+          std::make_shared<Node>(std::move(key), std::move(value), place);
+    }
+  }
+
+  value_type& at(key_type&& key) {
+    auto search_result = find(key);
+    if (key == search_result->key) {
+      return search_result->value;
+    }
+    throw std::out_of_range("No node found for the given key.");
+  }
+
+  const value_type& at(key_type&& key) const {
+    auto search_result = find(key);
+    if (key == search_result->key) {
+      return search_result->value;
+    }
+    throw std::out_of_range("No node found for the given key.");
+  }
 
  private:
   struct Node {
     key_type key = {};
     value_type value = {};
-    Node *parent = nullptr;
-    Node *left = nullptr;
-    Node *right = nullptr;
+    std::weak_ptr<Node> parent;
+    std::shared_ptr<Node> left;
+    std::shared_ptr<Node> right;
+
+    Node(key_type&& k, value_type&& v, std::weak_ptr<Node> p = {},
+         std::shared_ptr<Node> l = {}, std::shared_ptr<Node> r = {})
+        : key(std::move(k)),
+          value(std::move(v)),
+          parent(std::move(p)),
+          left(std::move(l)),
+          right(std::move(r)) {}
   };
 
-  Node *root = nullptr;
+  /// Searches for a Node with the given key or one that would become the parent
+  /// of the Node if the node with the given key were added now
+  std::shared_ptr<Node> find(key_type key) {
+    auto current = root;
+    while (current != nullptr) {
+      if (key == current->key) {
+        break;
+      }
+      if (cmp(key, current->key)) {
+        if (current->left != nullptr) {
+          current = current->left;
+        } else {
+          break;
+        }
+      } else {
+        if (current->right != nullptr) {
+          current = current->right;
+        } else {
+          break;
+        }
+      }
+    }
+    return current;
+  }
 
+  std::shared_ptr<Node> root;
+  Cmp cmp = Cmp();
 };
-
-
 
 #endif  // INCLUDE_BINARY_SEARCH_TREE_HPP_
--- tests/binary_search_tree_test.cpp
+++ tests/binary_search_tree_test.cpp
@@ -1,11 +1,17 @@
 // Copyright 2021 Your Name <your_email>
 
-#include <stdexcept>
-
 #include <gtest/gtest.h>
 
 #include <binary_search_tree.hpp>
+#include <stdexcept>
 
-TEST(Example, EmptyTest) {
-    EXPECT_THROW(example(), std::runtime_error);
+TEST(BST, BasicInsert) {
+  BinarySearchTree<int, std::string> tree;
+  tree.insert(1, "1");
+  tree.insert(3, "3");
+  EXPECT_EQ(tree.at(1), "1");
+  EXPECT_EQ(tree.at(3), "3");
+  EXPECT_NE(tree.at(3), "2");
+  EXPECT_THROW(tree.at(2), std::out_of_range);
+  EXPECT_THROW(tree.insert(3, "2"), std::runtime_error);
 }
