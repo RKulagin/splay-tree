95fdc519122cf716f2994bedf0f7401e0818c76f
RKulagin <rus.kulagin2001@yandex.ru>
2021-11-05 11:44:31 +0300

    Created BST dummy class and struct for Node

--- include/binary_search_tree.hpp
+++ include/binary_search_tree.hpp
@@ -3,7 +3,28 @@
 #ifndef INCLUDE_BINARY_SEARCH_TREE_HPP_
 #define INCLUDE_BINARY_SEARCH_TREE_HPP_
 
+template<class Key, class Value, class Cmp = std::less<Key>>
+class BinarySearchTree {
+ public:
+  using key_type = Key;
+  using value_type = Value;
 
-auto example() -> void;
+  // TODO: rule of five
+
+
+ private:
+  struct Node {
+    key_type key = {};
+    value_type value = {};
+    Node *parent = nullptr;
+    Node *left = nullptr;
+    Node *right = nullptr;
+  };
+
+  Node *root = nullptr;
+
+};
+
+
 
 #endif  // INCLUDE_BINARY_SEARCH_TREE_HPP_
